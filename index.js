6d18967efa648e117bb908cb946f298374f5c76a
dancun616 <dancun616@gmail.com>
2022-12-04 20:13:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let fetcher = fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => response.json())
+  .then((books) => {
+    renderBooks(books)
+  })
+  return fetcher 
 }
 
 function renderBooks(books) {
